9ca18e30b99a38ea947d4ac9d1bd101ca8ee2e23
Bothaina-Karakrah <63556283+Bothaina-Karakrah@users.noreply.github.com>
2020-05-29 20:26:40 +0300

    done

---
+++ Barrier.cpp
@@ -0,0 +1,49 @@
+#include "Barrier.h"
+#include <semaphore.h>
+
+Barrier::Barrier(unsigned int NumThreads): threadsNum(NumThreads), currCounter(0), state(true) {
+    sem_init(&mutex, 0, 1);
+    sem_init(&sem1, 0, 0);
+    sem_init(&sem2, 0, 0);
+}
+
+void Barrier::wait() {
+    if(this->state){
+        sem_wait(&this->mutex);
+        this->currCounter++;
+        if(this->currCounter == this->threadsNum){
+            for (int i = 0; i < this->threadsNum; ++i) {
+                sem_post(&sem1);
+                this->state = false;
+            }
+        }
+        sem_post(&mutex);
+        sem_wait(&sem1);
+    }
+    else if(!this->state){
+        sem_wait(&mutex);
+        this->currCounter--;
+        if(this->currCounter == 0){
+            for (int i = 0; i < this->threadsNum; ++i) {
+                sem_post(&sem2);
+                this->state = true;
+            }
+        }
+        sem_post(&mutex);
+        sem_wait(&sem2);
+    }
+    return;
+}
+
+unsigned int Barrier::waitingThreads() {
+    if(!this->state){
+        return (this->threads - this->curr);
+    }
+    return this->curr;
+}
+
+Barrier::~Barrier() {
+    sem_destroy(&mutex);
+    sem_destroy(&temp1);
+    sem_destroy(&temp2);
+}
