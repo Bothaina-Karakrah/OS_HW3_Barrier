8f497135521b88f89c1cd02f2c64c4dc91826df3
Bothaina-Karakrah <63556283+Bothaina-Karakrah@users.noreply.github.com>
2020-06-06 21:12:02 +0300

    change it

--- Barrier.cpp
+++ Barrier.cpp
@@ -1,37 +1,33 @@
 #include "Barrier.h"
 #include <semaphore.h>
 
-Barrier::Barrier(unsigned int num_of_threads): threadCounter(num_of_threads), currThreads(0), state(true) {
+Barrier::Barrier(unsigned int num_of_threads): threadCounter(num_of_threads), currThreads(0){
     sem_init(&mutex, 0, 1);
     sem_init(&sem1, 0, 0);
     sem_init(&sem2, 0, 0);
 }
 
 void Barrier::wait() {
-    if(this->state){
-        sem_wait(&this->mutex);
-        this->currThreads++;
-        if(this->currThreads == this->threadCounter){
-            for (unsigned int i = 0; i < this->threadCounter; ++i) {
-                sem_post(&sem1);
-                this->state = false;
-            }
+    sem_wait(&mutex);
+    currThreads++;
+    if(currThreads == threadCounter){
+        for (unsigned int i = 0; i < threadCounter; ++i) {
+            sem_post(&sem1);
         }
-        sem_post(&mutex);
-        sem_wait(&sem1);
     }
-    else if(!this->state){
-        sem_wait(&mutex);
-        this->currThreads--;
-        if(this->currThreads == 0){
-            for (unsigned int i = 0; i < this->threadCounter; ++i) {
-                sem_post(&sem2);
-                this->state = true;
-            }
+    sem_post(&mutex);
+    sem_wait(&sem1);
+
+
+    sem_wait(&mutex);
+    currThreads--;
+    if(currThreads == 0){
+        for (unsigned int i = 0; i < threadCounter; ++i) {
+            sem_post(&sem2);
         }
-        sem_post(&mutex);
-        sem_wait(&sem2);
     }
+    sem_post(&mutex);
+    sem_wait(&sem2);
     return;
 }
 
@@ -45,8 +41,5 @@ Barrier::~Barrier() {
 
 /////fot test
 unsigned int Barrier::waitingThreads() {
-    if(!this->state){
-        return (this->threadCounter - this->currThreads);
-    }
     return this->currThreads;
 }
